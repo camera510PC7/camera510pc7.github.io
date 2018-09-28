6f7cfbae887ce7f9756030f90378e6811494dad6
camera510PC7 <mjgjolmxmjgjokmjg_w_w@yahoo.co.jp>
2018-09-28 13:51:31 +0000

    ver1.0

--- js/index.js
+++ js/index.js
@@ -32,73 +32,79 @@ window.onload=function(){
     }
     
     $('.works_img').click(function(){
-        $("#back_black").css({'z-index':'15','display':'inline'});
 
         var img_name=$(this).attr("id");
         var modal_name=img_name.slice(0,-4);
-        
-        $('.modal_img').attr('src','img/works/'+modal_name+'.png');
-        
-        $('#'+modal_name).css({'z-index':'20','display':'inline'});
 
+
+            $('#'+modal_name).css({'z-index':'20','display':'inline'});
+            $("#back_black").css({'z-index':'15','display':'inline'});
     });
     
     $('.close').click(function(){
         
-        document.querySelector("[data-crno]").dataset.crno=1;
-        
         $('.works_description').css({'z-index':'0','display':'none'})
         
         $("#back_black").css({'z-index':'0','display':'none'});
-        
-        $('.go_right').css('display','inline');
-       
-       $('.go_left').css('display','none');
     });
 
-    $('.go_right').click(function(){
-        //document.querySelectorは一致する最初のElementしか返さないためバグの原因となる。修正の必要あり。
-        //修正内容:document.querySelectorを消去し、<img>にidを設定して.dataでカスタムデータを取得する。
-        var cr_no=Number(document.querySelector("[data-crno]").dataset.crno);
-        var current_img;
-        if(cr_no==1){
-            current_img=$('.modal_img').attr('src').slice(0,-4);
-        }else{
-            current_img=$('.modal_img').attr('src').slice(0,-5);
-        }
-        
-        var next_no=cr_no+1;
-        var max_no=Number(document.querySelector("[data-maxno]").dataset.maxno);
-        
-        if(next_no==2){
-            $('.go_left').css('display','inline');
-        }
-        if(next_no==max_no){
-            $('.go_right').css('display','none');
-        }
-        
-        $('.modal_img').attr('src',current_img+next_no+'.png');
-        document.querySelector("[data-crno]").dataset.crno=next_no;
+    
+    $(document).ready(function(){
+        $("#contact_form").validate({
+            rules:{
+                "entry.348163176":{
+                    required:true
+                },
+                "emailAddress":{
+                    required:true,
+                    email:true
+                },
+                "entry.1316222649":{
+                    required:true
+                }
+            },
+            messages:{
+                "entry.348163176":{
+                    required:"<font color=#F78181>名前を入力してください</font><br>"
+                },
+                "emailAddress":{
+                    required:"<font color=#F78181>メールアドレスを入力してください</font><br>",
+                    email:"<font color=#F78181>正しいメールアドレスを入力してください</font><br>"
+                },
+                "entry.1316222649":{
+                    required:"<font color=#F78181>内容を入力してください</font><br>"
+                }
+            },
+            submitHandler:function(contact_form){
+                contact_form.submit();
+            },
+            errorPlacement:function(err,elem){
+                err.appendTo($('#errer'));
+            }
+            
+        });
     });
     
-    $('.go_left').click(function(){
-        var current_img=$('.modal_img').attr('src').slice(0,-5);
-        var back_no=Number(document.querySelector("[data-crno]").dataset.crno)-1;
-        var max_no=Number(document.querySelector("[data-maxno]").dataset.maxno);
-        
-        if(back_no==max_no-1){
-            $('.go_right').css('display','inline');
-        }
-        
-        document.querySelector("[data-crno]").dataset.crno=back_no;
-        
-        if(back_no==1){
-            $('.go_left').css('display','none');
-            back_no="";
-        }
-        
-        $('.modal_img').attr('src',current_img+back_no+'.png');
-        
-    });
-    
+$(document).ready(function(){
+   $(".drawer").drawer({
+       class:{
+           drawerWidth:'100px'
+       }
+   }); 
+});
 };
+
+var submitted=false;
+function submit_page_change(){
+    if(submitted) {
+        alert("送信しました");
+        document.getElementById('name_imput').value="";
+        document.getElementById('e-mail_imput').value="";
+        document.getElementById('contact-content_imput').value="";
+    }
+}
+    
+function sp_menu_transition(){
+    $('.drawer').drawer('close');
+    this.blur();
+}
